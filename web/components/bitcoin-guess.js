db8dca11691798ac0f01535c2a9e6b552dea184f
Patrik Gustafsson <git@mumma.nu>
2025-08-08 14:12:11 +0200

    Add Vite configuration and package.json for development setup

--- web/components/bitcoin-guess.js
+++ web/components/bitcoin-guess.js
@@ -1,9 +1,9 @@
-import {define} from 'hybrids';
+import { html, define } from "https://unpkg.com/hybrids@%5E9";
 
 export default define({
   tag: 'bitcoin-guess',
-  render: function (host) {
-      return `
+  render: function () {
+      return html`
     <h2>Make Your Guess</h2>
     <button id="guess-up" type="button">Up</button>
     <button id="guess-down" type="button">Down</button>
@@ -11,4 +11,3 @@ export default define({
   `;
   },
 });
-
